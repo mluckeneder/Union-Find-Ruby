34e7b727478b99c1c359bf62bc655125741e5692
Michael Luckeneder <michael@luckeneder.net>
2012-10-02 12:08:25 +0100

    quick union impementation

---
+++ quick-union.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+class QuickFind
+  def initialize(n)
+    @ids = []
+    0.upto(n-1) {|i| @ids[i] = i}
+  end
+  
+  def connected?(id1,id2)
+    @ids[id1] == @ids[id2]
+  end
+  
+  def union(id1,id2)
+    id_1, id_2 = @ids[id1], @ids[id2]
+    @ids.map! {|i| (i == id_1) ? id_2 : i }
+  end
+end
+
+# Test Case
+qf = QuickFind.new 10
+
+puts qf.connected?(1,4)
+
+qf.union(1,4)
+qf.union(4,5)
+
+puts qf.connected?(1,4)
+puts qf.connected?(5,1)
+puts qf.connected?(2,4)
